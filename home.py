febf5e865b3d13a14ebbaf276fd959e08ab93c8c
Aditya <adi.walia16@gmail.com>
2022-03-21 23:24:23 +0530

    final Commit

--- home.py
+++ home.py
@@ -1,9 +1,23 @@
+import mysql.connector
+from functools import partial
+from urllib.parse import uses_fragment
+from ttkwidgets import Table
 import tkinter as tk
 from tkinter import ttk
 import requests
+from datetime import date
+
 
 from stockInfo import stockInfo
 
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='123456'
+)
+mycursor = mydb.cursor()
+
+mycursor.execute('use tradejunction')
 
 f = open('stocks.txt', 'r')
 ff = ('Times', 12)
@@ -15,11 +29,40 @@ def gen():
         tk.messagebox.showerror('Error', 'Please Enter Symbol!')
 
     else:
-        stockInfo(var.get(),sym.get())
-        
+        stockInfo(frame2, var.get(), sym.get())
+        return None
+
+
+def buy():
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={Bsym.get()}&apikey=Y4OVHTX2WGRVU8H6"
+    r = requests.get(url)
+    print(Bsym.get())
+    print(vols.get())
+    data = r.json()
+
+    a = data["Time Series (Daily)"]
+    c = 0
+    stockVal = []
+    for date in a:
+        for value in a[date]:
+
+            stockVal.append(a[date][value])
+
+        c += 1
+        if c > 0:
+            break
+
+    price = stockVal[0]
+
+    mycursor.execute("insert into Buy (UserName,Symbol,Volume,AtPrice,BuyDate) values('" +
+                     str(u[0][4])+"','"+str(Bsym.get())+"',"+str(vols.get()) + ","+str(price)+",now())")
+
+    mydb.commit()
+
 
 def homeScreen(user):
-    global sym, var
+    global sym, var, u, root, frame2, vols, Bsym
+    u = user
     root = tk.Tk()
     root.geometry('1000x500')
     root.title('Trade Junction')
@@ -78,7 +121,7 @@ def homeScreen(user):
     for x in f:
         b = x.split()
         for stock in b:
-            url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BSE:{stock}&apikey=Y4OVHTX2WGRVU8H6"
+            url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey=Y4OVHTX2WGRVU8H6"
             r = requests.get(url)
             data = r.json()
             a = data["Time Series (Daily)"]
@@ -96,6 +139,8 @@ def homeScreen(user):
                 stock, stockVal[0], stockVal[1], stockVal[2], stockVal[3], stockVal[4]))
 
     sym = tk.StringVar()
+    Bsym = tk.StringVar()
+    vols = tk.IntVar()
     # STOCK INFO
     stockLabel = tk.Label(frame2, text="Get Information On Any Stock")
     stockLabel.config(font=('Courier', 15, 'bold', 'underline'), fg='black')
@@ -124,6 +169,10 @@ def homeScreen(user):
     R5 = tk.Radiobutton(frame2, text="Quarterly Earnings",
                         variable=var, value=5)
     R5.config(font=('Arial', 10))
+
+    R6 = tk.Radiobutton(frame2, text="Price Graph",
+                        variable=var, value=6)
+    R6.config(font=('Arial', 10))
     gen_btn = tk.Button(
         frame2,
         width=15,
@@ -140,5 +189,133 @@ def homeScreen(user):
     R3.place(x=45, y=150)
     R4.place(x=325, y=100)
     R5.place(x=325, y=150)
+    R6.place(x=520, y=150)
 
+    # BUY STOCK
+    buyLabel = tk.Label(frame3, text="Buy Stocks")
+    buyLabel.config(font=('Courier', 14, 'bold', 'underline'), fg='black')
+    buyLabel.place(x=350, y=15)
+
+    BsymbolLabel = tk.Label(frame3, text="Symbol:")
+    BsymbolLabel.config(font=('Courier', 12, 'bold'), fg='black')
+    BsymbolLabel.place(x=370, y=55)
+    BsymbolEntry = tk.Entry(frame3, bd=2, textvariable=Bsym, width=10)
+    BsymbolEntry.place(x=450, y=55)
+
+    volLabel = tk.Label(frame3, text="Volume:")
+    volLabel.config(font=('Courier', 12, 'bold'), fg='black')
+    volLabel.place(x=370, y=155)
+
+    volEntry = tk.Entry(frame3, bd=2, textvariable=vols, width=10)
+    volEntry.place(x=450, y=155)
+
+    buy_btn = tk.Button(
+        frame3,
+        width=15,
+        text='Buy',
+        font=ff,
+        relief=tk.SOLID,
+        cursor='hand2',
+        command=buy
+    )
+
+    buy_btn.place(x=725, y=100)
+
+    # PORTFOLIO
+    temp=[]
+    sortable = tk.BooleanVar(root, False)
+    drag_row = tk.BooleanVar(root, False)
+    drag_col = tk.BooleanVar(root, False)
+    table = Table(frame4,sortable=sortable.get(), drag_cols=drag_col.get(),
+                    drag_rows=drag_row.get(), height=6)
+    mycursor.execute("select * from Buy where UserName='"+str(u[0][4])+"'")
+    result = mycursor.fetchall()
+    num_fields = len(mycursor.description)
+    field_names = [i[0] for i in mycursor.description]
+    table.config(columns=field_names)
+    
+    if len(result) == 0:
+        noLabel = tk.Label(frame4, text="No Stocks In Portfolio")
+        noLabel.config(font=('Courier', 14, 'bold', 'underline'), fg='black')
+        noLabel.place(x=350, y=15)
+        
+    
+    for x in field_names:
+        table.heading(x, text=x)
+        table.column(x, width=100, stretch=False)
+
+    for x in result:
+        for a in x:
+            temp.append(a)
+
+        table.insert("", 'end', text="L1", values=tuple(temp))
+        temp = []
+
+    sx = tk.Scrollbar(frame4, orient='horizontal', command=table.xview)
+    sy = tk.Scrollbar(frame4, orient='vertical', command=table.yview)
+    table.configure(yscrollcommand=sy.set, xscrollcommand=sx.set)
+    table.grid(sticky='ewns')
+    sx.grid(row=1, column=0, sticky='ew')
+    sy.grid(row=0, column=1, sticky='ns')
+    frame4.update_idletasks()
+    # toggle table properties
+
+    def toggle_sort():
+        table.config(sortable=sortable.get())
+
+    def toggle_drag_col():
+        table.config(drag_cols=drag_col.get())
+
+    def toggle_drag_row():
+        table.config(drag_rows=drag_row.get())
+
+    frame = tk.Frame(frame4)
+    tk.Checkbutton(frame, text='sortable', variable=sortable,
+                    command=toggle_sort).pack(side='left')
+    tk.Checkbutton(frame, text='drag columns', variable=drag_col,
+                    command=toggle_drag_col).pack(side='left')
+    tk.Checkbutton(frame, text='drag rows', variable=drag_row,
+                    command=toggle_drag_row).pack(side='left')
+    frame.grid()
+    
+    
+    proLabel = tk.Label(frame5, text="Your Profile")
+    proLabel.config(font=('Courier', 14, 'bold', 'underline'), fg='black')
+    proLabel.place(x=350, y=15)
+    
+    nameLabel = tk.Label(frame5, text="Name:")
+    nameLabel.config(font=('Courier', 14, 'bold', 'underline'), fg='black')
+    nameLabel.place(x=30, y=55)
+    name = tk.Label(frame5, text=str(u[0][1])+" " + str(u[0][2]))
+    name.config(font=('Courier', 13, 'bold'), fg='black')
+    name.place(x=110, y=55)
+    
+    mailLabel = tk.Label(frame5, text="Email:")
+    mailLabel.config(font=('Courier', 14, 'bold', 'underline'), fg='black')
+    mailLabel.place(x=30, y=105)
+    mail = tk.Label(frame5, text=str(u[0][7]))
+    mail.config(font=('Courier', 13, 'bold'), fg='black')
+    mail.place(x=110, y=105)
+    
+    mobiLabel = tk.Label(frame5, text="Ph No:")
+    mobiLabel.config(font=('Courier', 14, 'bold', 'underline'), fg='black')
+    mobiLabel.place(x=30, y=155)
+    mobi = tk.Label(frame5, text=str(u[0][8]))
+    mobi.config(font=('Courier', 13, 'bold'), fg='black')
+    mobi.place(x=110, y=155)
+    
+    addrLabel = tk.Label(frame5, text="Address:")
+    addrLabel.config(font=('Courier', 14, 'bold', 'underline'), fg='black')
+    addrLabel.place(x=300, y=155)
+    addr = tk.Label(frame5, text=str(u[0][6]))
+    addr.config(font=('Courier', 13, 'bold'), fg='black')
+    addr.place(x=400, y=155)
+    
+    
+    
+    
+    
+    
+    
+    
     root.mainloop()
--- stockInfo.py
+++ stockInfo.py
@@ -1,11 +1,87 @@
+from ttkwidgets import Table
 import tkinter as tk
 from tkinter import ttk
-from ttkwidgets import Table
 import requests
+import matplotlib.pyplot as plt
 
 
-def stockInfo(choice, sym):
+def delete_command(tree,root):
+    print('sdf')
+    tree.destroy()
+    root.destroy()
+    
 
+def fetchData(url, keys, sym):
+
+    r = requests.get(url)
+    data = r.json()
+    cols = []
+    temp = []
+
+    if (len(data) == 1):
+        tk.messagebox.showerror('Error', 'Please Wait!')
+    else:
+        for rec in data[keys]:
+            for names in rec:
+                if names not in cols:
+                    cols.append(names)
+        table.config(columns=cols)
+        for x in cols:
+            table.heading(x, text=x)
+            table.column(x, width=100, stretch=False)
+
+        for x in data[keys]:
+            for vals in x:
+                temp.append(x[vals])
+
+            table.insert("", 'end', text="L1", values=tuple(temp))
+            temp = []
+
+        sx = tk.Scrollbar(root, orient='horizontal', command=table.xview)
+        sy = tk.Scrollbar(root, orient='vertical', command=table.yview)
+        table.configure(yscrollcommand=sy.set, xscrollcommand=sx.set)
+        table.grid(sticky='ewns')
+        sx.grid(row=1, column=0, sticky='ew')
+        sy.grid(row=0, column=1, sticky='ns')
+        root.update_idletasks()
+        # toggle table properties
+
+        def toggle_sort():
+            table.config(sortable=sortable.get())
+
+        def toggle_drag_col():
+            table.config(drag_cols=drag_col.get())
+
+        def toggle_drag_row():
+            table.config(drag_rows=drag_row.get())
+
+        frame = tk.Frame(root)
+        tk.Checkbutton(frame, text='sortable', variable=sortable,
+                       command=toggle_sort).pack(side='left')
+        tk.Checkbutton(frame, text='drag columns', variable=drag_col,
+                       command=toggle_drag_col).pack(side='left')
+        tk.Checkbutton(frame, text='drag rows', variable=drag_row,
+                       command=toggle_drag_row).pack(side='left')
+        frame.grid()
+        root.geometry('400x200')
+        root.protocol("WM_DELETE_WINDOW",lambda: delete_command(table,root))
+        root.mainloop()
+        return None
+
+
+def stockInfo(win,choice, sym):
+    global root,table,drag_col,drag_row,sortable
+    root = tk.Toplevel(win)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.title(sym)
+    
+    sortable = tk.BooleanVar(root, False)
+    drag_row = tk.BooleanVar(root, False)
+    drag_col = tk.BooleanVar(root, False)
+    table = Table(root,sortable=sortable.get(), drag_cols=drag_col.get(),
+                    drag_rows=drag_row.get(), height=6)
+   
     if choice == 1:
 
         url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={sym}&apikey=HYGXZH658SIG5BL0'
@@ -17,21 +93,12 @@ def stockInfo(choice, sym):
         if (len(data) == 1):
             tk.messagebox.showerror('Error', 'Please Wait!')
         else:
-            root = tk.Tk()
-            root.columnconfigure(0, weight=1)
-            root.rowconfigure(0, weight=1)
-            root.title(sym)
-            style = ttk.Style(root)
-            style.theme_use('alt')
-            sortable = tk.BooleanVar(root, False)
-            drag_row = tk.BooleanVar(root, False)
-            drag_col = tk.BooleanVar(root, False)
+            
 
             for x in data:
                 cols.append(x)
-            table = Table(root, columns=cols, sortable=sortable.get(), drag_cols=drag_col.get(),
-                          drag_rows=drag_row.get(), height=6)
-        
+            
+            table.config(columns=cols)
             for x in cols:
                 table.heading(x, text=x)
                 table.column(x, width=100, stretch=False)
@@ -49,28 +116,72 @@ def stockInfo(choice, sym):
             root.update_idletasks()
             # toggle table properties
 
-
             def toggle_sort():
                 table.config(sortable=sortable.get())
-
 
             def toggle_drag_col():
                 table.config(drag_cols=drag_col.get())
 
-
             def toggle_drag_row():
                 table.config(drag_rows=drag_row.get())
 
-
             frame = tk.Frame(root)
             tk.Checkbutton(frame, text='sortable', variable=sortable,
-                        command=toggle_sort).pack(side='left')
+                           command=toggle_sort).pack(side='left')
             tk.Checkbutton(frame, text='drag columns', variable=drag_col,
-                        command=toggle_drag_col).pack(side='left')
+                           command=toggle_drag_col).pack(side='left')
             tk.Checkbutton(frame, text='drag rows', variable=drag_row,
-                        command=toggle_drag_row).pack(side='left')
+                           command=toggle_drag_row).pack(side='left')
             frame.grid()
             root.geometry('400x200')
 
             root.mainloop()
-            
+            return None
+
+    elif choice == 2:
+
+        fetchData(
+            f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={sym}&apikey=HYGXZH658SIG5BL0', 'quarterlyReports', sym)
+        return None
+
+    elif choice == 3:
+
+        fetchData(
+            f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={sym}&apikey=HYGXZH658SIG5BL0', 'quarterlyReports', sym)
+        return None
+    elif choice == 4:
+
+        fetchData(
+            f'https://www.alphavantage.co/query?function=CASH_FLOW&symbol={sym}&apikey=HYGXZH658SIG5BL0', 'quarterlyReports', sym)
+        return None
+
+    elif choice == 5:
+
+        fetchData(
+            f'https://www.alphavantage.co/query?function=EARNINGS&symbol={sym}&apikey=HYGXZH658SIG5BL0', 'quarterlyEarnings', sym)
+        return None
+    
+    else:
+        
+        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={sym}&apikey=Y4OVHTX2WGRVU8H6"
+        r = requests.get(url)
+        data = r.json()
+        a = data["Time Series (Daily)"]
+        c = 0
+        x = []
+        y = []
+        for date in a:
+            for value in a[date]:
+                x.append(date)
+                y.append(a[date]['4. close'])
+        
+  
+        
+        
+        
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.title(sym)
+        plt.plot(x,y)
+        plt.show()
+        
